2460ceffea116862c3518adfad6b26d90297599d
Krish Sajnani <krishsajnani13@gmail.com>
2023-04-20 14:57:23 +0530

    commit x

--- A6/Task2/app.js
+++ A6/Task2/app.js
@@ -1,22 +1,22 @@
 var b = document.getElementById("add");
-b.addEventListener('click', add);
+b.addEventListener("click", add);
 
 var a = document.getElementById("reset");
-a.addEventListener('click', reset);
+a.addEventListener("click", reset);
 
-function add(){
-var inp = document.getElementById("input").value;
-var newNode=document.createElement("li");
-var newText= document.createTextNode(inp);
-newNode.appendChild(newText);
-console.log(newNode)
-document.getElementById("list").appendChild(newNode);
+function add() {
+  var inp = document.getElementById("input").value;
+  var newNode = document.createElement("li");
+  var newText = document.createTextNode(inp);
+  newNode.appendChild(newText);
+  console.log(newNode);
+  document.getElementById("list").appendChild(newNode);
 }
 
-function reset(){
- var docElement;
- docElement = document.getElementById("list");
- while(docElement.hasChildNodes()){
- docElement.firstChild.remove();
- }
+function reset() {
+  var docElement;
+  docElement = document.getElementById("list");
+  while (docElement.hasChildNodes()) {
+    docElement.firstChild.remove();
+  }
 }
--- A6/Task3/app.js
+++ A6/Task3/app.js
@@ -1,28 +1,28 @@
 var b = document.getElementById("input");
-var inp=1;
+var inp = 1;
 add(inp);
 
-b.addEventListener('change',  () => {
- var li=document.getElementById("list")
- del(li);
- var inp = b.value;
- for (var i = 0; i < inp; i++) {
-  add(i+1);
- }
+b.addEventListener("change", () => {
+  var li = document.getElementById("list");
+  del(li);
+  var inp = b.value;
+  for (var i = 0; i < inp; i++) {
+    add(i + 1);
+  }
 });
 
 function add(inp) {
- var newNode = document.createElement("p");
- var newText = document.createTextNode("This is Para Number :" + inp);
- newNode.style.fontSize='25px';
- newNode.appendChild(newText);
- console.log(newNode);
- document.getElementById("list").appendChild(newNode);
+  var newNode = document.createElement("p");
+  var newText = document.createTextNode("This is Para Number :" + inp);
+  newNode.style.fontSize = "25px";
+  newNode.appendChild(newText);
+  console.log(newNode);
+  document.getElementById("list").appendChild(newNode);
 }
 
 function del(li) {
- console.log("del")
- while (li.firstChild) {
-  li.removeChild(li.firstChild)
- }
+  console.log("del");
+  while (li.firstChild) {
+    li.removeChild(li.firstChild);
+  }
 }
